6072f7de36db67b4719574d0ba41c35fa6cb5975
Brian Sai <brianwsai@gmail.com>
2021-05-15 11:30:28 -0700

    Implemented logic to listen on submit to add data to firestore, along with error handling

---
+++ index.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const cost = document.querySelector('#cost');
+const error = document.querySelector('#error');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const costValue = parseInt(cost.value);
+
+  if (!name.value || !cost.value) {
+    error.textContent = 'Please enter values before submitting';
+  } else if (!costValue) {
+    error.textContent = 'Please enter a valid number';
+  } else {
+    const item = {
+      name: name.value,
+      cost: costValue,
+    };
+    db.collection('expenses')
+      .add(item)
+      .then((res) => {
+        name.value = '';
+        cost.value = '';
+        error.textContent = '';
+      });
+  }
+});
