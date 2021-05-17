5e91c2921b88b5db81783b94d8e86126a8e696fe
Brian Sai <brianwsai@gmail.com>
2021-05-17 15:44:12 -0700

    Created a colored graph that updates when a new item is added

---
+++ graph.js
@@ -0,0 +1,85 @@
+const dims = { height: 300, width: 300, radius: 150 };
+const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
+
+// create svg container
+const svg = d3
+  .select('.canvas')
+  .append('svg')
+  .attr('width', dims.width + 150)
+  .attr('height', dims.height + 150);
+
+const graph = svg
+  .append('g')
+  .attr('transform', `translate(${cent.x}, ${cent.y})`);
+// translates the graph group to the middle of the svg container
+
+const pie = d3
+  .pie()
+  .sort(null)
+  .value((d) => d.cost);
+// the value we are evaluating to create the pie angles
+
+const arcPath = d3
+  .arc()
+  .outerRadius(dims.radius)
+  .innerRadius(dims.radius / 2);
+
+// ordianl colour scale
+const colour = d3.scaleOrdinal(d3['schemeSet3']);
+
+// update function
+const update = (data) => {
+  // update colour scale domain
+  colour.domain(data.map((d) => d.name));
+
+  // join enhanced (pie) data to path elements
+  const paths = graph.selectAll('path').data(pie(data));
+
+  // handle the exit selection
+  paths.exit().remove();
+
+  // handle the current DOM path updates
+  paths.attr('d', arcPath);
+
+  paths
+    .enter()
+    .append('path')
+    .attr('class', 'arc')
+    //.attr('d', arcPath)
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 3)
+    .attr('fill', (d) => colour(d.data.name))
+    .transition()
+    .duration(750)
+    .attrTween('d', arcTweenEnter);
+};
+
+// data array and firestore
+var data = [];
+
+db.collection('expenses').onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+    const dataAction = {
+      added: () => data.push(doc),
+      modified: () => {
+        const index = data.findIndex((item) => item.id === doc.id);
+        data[index] = doc;
+      },
+      removed: () => {
+        data = data.filter((item) => item.id !== doc.id);
+      },
+    };
+    dataAction[change.type]();
+    update(data);
+  });
+});
+
+const arcTweenEnter = (d) => {
+  var i = d3.interpolate(d.endAngle - 0.1, d.startAngle);
+
+  return function (t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
